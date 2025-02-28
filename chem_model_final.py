f42808140a21c935c5f22d15b9f274662e02dccb
Aalmond04 <ryotakamikura0616@gmail.com>
2025-02-28 13:37:16 +0900

    update

---
+++ chem_model_final.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+plt.style.use('default')
+plt.rc('font', family='Cambria Math')
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.serif'] = ['Cambria Math'] + plt.rcParams['font.serif']
+plt.rcParams['figure.figsize'] = (8, 5)
+def first_order_reaction(t, y, k):
+    """Returns the derivative of [A] with respect to time for a first-order reaction."""
+    A = y[0]
+    dAdt = -k * A
+    return [dAdt]
+k = 0.5e-4     # rate
+A0 = 0.1    # initial concentration of A
+t_span = (0, 100)  # Time range
+solution_1st = solve_ivp(
+    fun=lambda t, y: first_order_reaction(t, y, k),
+    t_span=t_span,
+    y0=[A0],
+    t_eval=np.linspace(t_span[0], t_span[1], 100)
+)
+
+t_vals_1st = solution_1st.t
+A_vals_1st = solution_1st.y[0]
+plt.figure()
+plt.plot(t_vals_1st, A_vals_1st, label='[A](t)')
+plt.xlabel('Time')
+plt.ylabel('Concentration')
+plt.title('First-Order Reaction')
+plt.legend()
+plt.show()
+
